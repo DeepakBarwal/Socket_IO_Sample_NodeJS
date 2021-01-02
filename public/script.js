f49def19e99e7e5dd89ca776475ad58eb1d74726
DeepakBarwal <deepakbarwal98@gmail.com>
2021-01-02 18:57:43 +0530

    user auth added

--- public/script.js
+++ public/script.js
@@ -1,1 +1,32 @@
 let socket = io();
+
+$('#loginBox').show();
+$('#chatBox').hide();
+
+$('#btnStart').click(() => {
+  socket.emit('login', {
+    username: $('#inpUsername').val(),
+    password: $('#inpPassword').val(),
+  });
+});
+
+socket.on('logged_in', () => {
+  $('#loginBox').hide();
+  $('#chatBox').show();
+});
+
+socket.on('login_failed', () => {
+  window.alert('wrong password');
+});
+
+$('#btnSendMsg').click(() => {
+  socket.emit('msg_send', {
+    to: $('#inpToUser').val(),
+    msg: $('#inpNewMsg').val(),
+  });
+  $('#inpNewMsg').val('');
+});
+
+socket.on('msg_rcvd', (data) => {
+  $('#ulMsgs').append($('<li>').text(`[${data.from}] : ${data.msg}`));
+});
--- server.js
+++ server.js
@@ -6,8 +6,48 @@ const socketio = require('socket.io');
 const server = http.createServer(app);
 const io = socketio(server);
 
+let users = {
+  yogesh: 'gujjarbadshah',
+};
+
+let socketMap = {};
+
 io.on('connection', (socket) => {
   console.log('connected to socket with id ', socket.id);
+
+  function login(s, u) {
+    s.join(u);
+    s.emit('logged_in');
+    socketMap[s.id] = u;
+    console.log(socketMap);
+  }
+
+  socket.on('login', (data) => {
+    if (users[data.username]) {
+      if (users[data.username] === data.password) {
+        login(socket, data.username);
+      } else {
+        socket.emit('login_failed');
+      }
+    } else {
+      users[data.username] = data.password;
+      login(socket, data.username);
+    }
+  });
+
+  socket.on('msg_send', (data) => {
+    data.from = socketMap[socket.id];
+    if (data.to) {
+      io.to(data.to).emit('msg_rcvd', data);
+    } else {
+      socket.broadcast.emit('msg_rcvd', data);
+    }
+  });
+  // socket.on('msg_send', (data) => {
+  //   io.emit('msg_rcvd', data); // send to all sockets
+  //   // socket.emit('msg_rcvd', data); // send to only 1 socket(sender itself)
+  //   // socket.broadcast.emit('msg_rcvd', data); // everyone except sender
+  // });
 });
 
 app.use('/', express.static(__dirname + '/public'));
